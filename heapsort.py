c3c1a51ce95e20b7362e277723d0082f85c161cb
Zu <zuzanna.misztal.agh@gmail.com>
2017-12-05 14:04:57 +0100

    Reimplementation of heapsort

--- heapsort.py
+++ heapsort.py
@@ -1,12 +1,12 @@
 import random
 tablica=[]
-a=10
+a=100
 for i in range(a):
 	tablica.append(i)
 random.shuffle(tablica)
 print (tablica)
 
-def heapify(tab, x, size):
+def heapify(tab, x, size): #x to index
 	left=2*x+1
 	right=2*x+2
 	largest=x
@@ -20,15 +20,14 @@ def heapify(tab, x, size):
 	
 def build_heap(tab):
 	heap_size=len(tab)
-	for j in range(int((len(tab)-1)/2),-1,-1):
+	for j in range(int((len(tab)-1)/2),-1,-1):#j to indeksy idąc od "dołu" kopca z pominięciem liści
 		heapify(tab, j, heap_size)
 
 def heapsort(tab):
 	build_heap(tab)
 	heap_size=len(tab)
 	for k in range(len(tab)-1,0,-1):
-		tmp=tab.pop(0)
-		tab.insert(k,tmp)
+		tab[k],tab[0]=tab[0],tab[k]
 		heap_size=heap_size-1
 		heapify(tab,0,heap_size)
 		
